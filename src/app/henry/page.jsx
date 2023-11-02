cfeafb844d18c8c16cd9b4f83552ee20c5e674f6
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-02 09:53:59 +0100

    commit first

---
+++ src/app/henry/page.jsx
@@ -0,0 +1,13 @@
+export default async function Henry() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  const data = await res.json();
+  const { name, favouriteColor, age, image } = data;
+  return (
+    <main>
+      <h1>This is {data.name}</h1>
+      <p>His favorite color is : {data.favouriteColor}</p>
+      <p>He is {data.age} years old</p>
+      <img src={data.image} alt="picture of henry here"></img>
+    </main>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./Style.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={styles.h1}>Hej</h1>
+    </main>
+  );
+}
